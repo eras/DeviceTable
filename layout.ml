e81c574e794f8ad763306bae0205839c56187613
Erkki Seppälä <flux@modeemi.fi>
2015-05-18 22:35:01 +0300

    Layout: added a whole lot of comments

--- layout.ml
+++ layout.ml
@@ -2,6 +2,8 @@ open Util
 
 module P = Containers_misc.PrintBox
 
+(* Given a matrix of values, for each column find the maximum value
+   and put them in order to result list *)
 let max_widths xs =
   transpose xs
   |> List.map (List.fold_left max 0)
@@ -12,18 +14,25 @@ let box_width box =
 let box_height box =
   (P.Box.size box).P.y
 
+(* [rpad n box] pads an object by adding n spaces to its right side *)
 let rpad n box =
   let open Containers_misc.PrintBox in
   hlist ~bars:false [box; text (String.make n ' ')]
 
+(* return a box padded by its right side so that it is at least
+   required_width characters wide; if it's already large enough (or
+   larger), return the original box *)
 let adjust_box_width box required_width =
   let orig_width = (P.Box.size box).P.x in
   if orig_width >= required_width
   then box
   else rpad (required_width - orig_width) box
 
+(* for a list of boxes and an equally long list of widths, adjuste
+   each box width to match the required width *)
 let adjust_box_row_widths boxes required_widths = List.map2 adjust_box_width boxes required_widths
 
+(* for a list of boxes, adjust each box size so that their total width will be exactly max_widths *)
 let adjust_total_width max_width widths =
   let cur_width = sum widths in
   let difference = max_width - cur_width in
@@ -40,11 +49,14 @@ let adjust_total_width max_width widths =
     in
     loop [] widths
 
+(* produce a vertical bar as tall as "height" *)
 let vertical_bar height =
   CCList.range 1 height |> List.map (const (P.text "|")) |> P.vlist ~bars:false
 
+(* determine the maximum height of all boxes in a list of boxes *)
 let row_height row = row |> List.map box_height |> List.fold_left max 0
 
+(* on a row put a vertical bar to the left side of each box *)
 let row_frames row =
   let height = row_height row in
   let rec loop accu = function
@@ -53,6 +65,10 @@ let row_frames row =
   in
   loop [] row
 
+(* generate +---+ -kind of objects for a pair of rows so that +s go to
+   the places where even objects are (so it assumes each row has a bar
+   character at every other slot, beginning from the first element
+   being a bar). *)
 let middle_bars a b =
   let offsets = function
     | None -> []
@@ -82,14 +98,35 @@ let add_middle_bars boxes =
   | [] -> []
   | (x::_) as xs -> middle_bars None (Some x)::loop xs
 
+(* TODO: there is a problem width dealing with vertical bars..
+
+   as they too are aligned by the adjust_total_width function.
+
+   Either the functionality to avoid that should be added to the
+   adjust_total_width function, or the contents should be mapped very
+   soon into something like ([`Left | `Right | `Center | `NoExpand] *
+   P.Box.t) list which would be more general.
+*)
 let horizontal_lists rows =
   (* add left side edges to the boxes *)
   let rows = List.map row_frames rows in
+  (* for a list of boxes, get their widths *)
   let col_widths boxes = List.map (fun b -> (P.Box.size b).P.x) boxes in
+  (* acquire box widths of each row; group them by the number of boxes
+     in each row *)
   let row_widths = (rows |> List.map @@ fun boxes -> (List.length boxes, col_widths boxes)) |> group_by identity in
+  (* the desired width is the maximum size of each column for each
+     different-ary row. (ie. 3-box columns are considered completely
+     separately from 4-box columns) *)
   let adjusted_widths = row_widths |> List.map (project2nd max_widths) in
+  (* maximum width: the longest width taking the adjusted_widths
+     adjustment into account *)
   let max_width = adjusted_widths |> List.map snd |> List.map (sum) |> reduce max in
+  (* acquire new adjusted_widths by enlongening box widths so that
+     each row class (classified by the number of boxes in them)
+     becomes as long *)
   let adjusted_widths = adjusted_widths |> List.map (project2nd (adjust_total_width max_width)) in
+  (* finally produce the adjusted boxes using adjust_widths *)
   let adjusted_rows =
     rows
     |> List.map @@ fun boxes ->
@@ -97,9 +134,14 @@ let horizontal_lists rows =
     let height = row_height boxes in
     let adjusted_widths = List.assoc len adjusted_widths in
     adjust_box_row_widths boxes adjusted_widths |>
+    (* the right-most vertical bar was not taken into account
+       previously (so it wouldn't be aligned); add it here *)
     append (vertical_bar height)
   in
+  (* between each row (and before the first and after the last) add
+     the +----+ -rows *)
   let adjusted_rows = add_middle_bars adjusted_rows in
+  (* finally we have he layout*)
   let layout = P.vlist ~bars:false (adjusted_rows |> List.map @@ fun boxes ->
                                     (P.hlist ~bars:false boxes))
   in
